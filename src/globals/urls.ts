9167ca36294f74010a92201051e9870fcb5b7c54
LineHayat <linehayatest@gmail.com>
2021-09-17 20:22:42 +0800

    [FIX] urls

--- src/globals/urls.ts
+++ src/globals/urls.ts
@@ -2,7 +2,7 @@
 const BASE_URL = "https://linehayat-volunteer.vercel.app"
 
 // const SOCKET_URL = `ws://localhost:8050/ws`
-const SOCKET_URL = "wss://sheltered-thicket-36826.herokuapp.com"
+const SOCKET_URL = "wss://mighty-beach-78572.herokuapp.com"
 
 // const REST_URL = `http://localhost:8050`
 const REST_URL = `https://mighty-beach-78572.herokuapp.com`
