398a62116b721f39a0592a80cb4bfb1be43c980d
LineHayat <linehayatest@gmail.com>
2021-09-17 22:05:35 +0800

    [FIX] urls

--- src/globals/urls.ts
+++ src/globals/urls.ts
@@ -1,14 +1,15 @@
 // const BASE_URL = "http://localhost:3001"
+// const BASE_URL = "https://linehayat-website-2.vercel.app"
 const BASE_URL = "https://linehayat-volunteer.vercel.app"
 
+const SOCKET_URL = "wss://https://linehayat-server-1.herokuapp.com/ws"
 // const SOCKET_URL = `ws://localhost:8050/ws`
-const SOCKET_URL = "wss://mighty-beach-78572.herokuapp.com"
 
+const REST_URL = `https://linehayat-server-1.herokuapp.com`
 // const REST_URL = `http://localhost:8050`
-const REST_URL = `https://mighty-beach-78572.herokuapp.com`
 
 export {
   BASE_URL,
   SOCKET_URL,
-  REST_URL
+  REST_URL,
 }
